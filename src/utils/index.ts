d067264cb08bb39ada7d02f3e4ba9303c538d628
Hokori23 <hokori23@qq.com>
2021-03-20 15:04:19 +0800

    feat: Tree test => create & setNull

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,14 +7,6 @@ export const isFlat = (arr: any[]): boolean => {
 	return true
 }
 
-export const countFlat = (arr: any[]): number => {
-	let res = 0
-	for (var i = 0; i < arr.length; i++) {
-		if (!Array.isArray(arr[i])) res++
-	}
-	return res
-}
-
 export const isDef = (v: any): boolean => {
 	return v !== undefined && v !== null
 }
